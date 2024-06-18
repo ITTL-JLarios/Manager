6c73a6f630d6abd092ca8660bb84fa5b871a61ad
ittl-jlarios <jared@totaltransportlogistics.us>
2024-06-18 13:37:01 -0600

    feat: mkv recording with mss

--- program/screen_rec.py
+++ program/screen_rec.py
@@ -1,14 +1,15 @@
 from program.service import Service, threaded
 
 from datetime import datetime, timedelta
-from PIL import ImageGrab
 import numpy as np
 import imageio
-import cv2
 import os
+import cv2
+import mss
+import time
 
 class ScreenRecording( Service ):
-    def __init__(self, base_dir='rec', fps=5, time_lapse=15, elderness=7):
+    def __init__(self, base_dir='rec', fps=30, time_lapse=1, elderness=7):
         '''
         args:
             base_dir: Base directory for input or ouput files
@@ -21,53 +22,34 @@ class ScreenRecording( Service ):
         self.elderness = elderness
 
 
+    def get_combined_screenshot():
+        # Get information about all monitors
+        pass
+
+            
+
     def record(self):
         '''
             Screen Recording as service
         '''
         init_time = datetime.now()
         end_time = init_time + timedelta(minutes=self.time_lapsus)
+        with mss.mss() as sct:
+            monitor = sct.monitors[1]  # Capture the primary monitor
+            fourcc = cv2.VideoWriter_fourcc(*'VP80')  # VP8 codec for .webm format
+            out = cv2.VideoWriter(
+                os.path.join(self.base_path,f'{init_time.date()}_{init_time.hour}_{init_time.minute}.mkv'), 
+                fourcc, 
+                self.fps, 
+                (monitor['width'], monitor['height']))
 
-        # Obtain image dimensions 
-        # Screen capture
-        image = ImageGrab.grab(all_screens=True)
+       
+            while datetime.now() < end_time:
+                img = np.array(sct.grab(monitor))
+                frame = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+                out.write(frame)
 
-        # Convert the object to numpy array
-        img_np_arr = np.array(image)
-
-        # Extract and print shape of array
-        shape = img_np_arr.shape
-
-        # Low scale_by_percent implies smaller window
-        height = shape[0] + (16 - shape[0] % 16) if shape[0] % 16 != 0 else shape[0]
-        width = shape[1] + (16 - shape[1] % 16) if shape[1] % 16 != 0 else shape[1]
-    
-        new_dim = (width, height)
-
-        # Create a video writer
-        screen_cap_writer = imageio.get_writer(
-            os.path.join(self.base_path,f'{init_time.date()}_{init_time.hour}_{init_time.minute}.webm'),
-            fps=self.fps,
-            codec='vp8')
-        
-        # Screen Recording
-        while True:
-            # Capture screen
-            image = ImageGrab.grab(all_screens=True)
-            # Convert to array
-            img_np_arr = np.array(image)
-            # Video Scaling
-            final_img = cv2.resize(img_np_arr, new_dim)
-            # Write to video
-            screen_cap_writer.append_data(final_img)
-
-            # Stop and exit screen recording if tiem lapse is past
-            if datetime.now() >= end_time:
-                break
-
-        # Release the created the objects
-        screen_cap_writer.close()
-        cv2.destroyAllWindows()
+            out.release()
 
 
     def delete_old_rec(self):
